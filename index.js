822d1869095b7b047fbf17b61b7fcbc53298a151
Ayaz <ayazcw@gmail.com>
2022-04-25 13:48:10 +0400

    index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+
+function links_to_namedlink(text) {
+    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(urlRegex, function(url) {
+        let last = url.split('/').pop();
+        return '<' + url + ' | ' + last + '>';
+    });
+}
+
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const in_text = core.getInput('in_text');
+    core.setOutput("modified_text", links_to_namedlink(in_text));
 } catch (error) {
     core.setFailed(error.message);
 }
